b8ef6376c4950e9b53b0a510478c59dbb47fbe16
Laurynas Butkus <laurynas.butkus@gmail.com>
2014-07-03 20:47:20 +0300

    Tracking number validator

--- lib/royal_mail.rb
+++ lib/royal_mail.rb
@@ -6,4 +6,11 @@ require "royal_mail/tracker/detail"
 require "royal_mail/tracker/status_map"
 
 module RoyalMail
+  TRACKING_NUMBER_FORMAT = /\A[A-Z]{2}\d{9}GB\z/i
+
+  class << self
+    def tracking_number?(tracking_number)
+      !!(tracking_number.to_s =~ TRACKING_NUMBER_FORMAT)
+    end
+  end
 end
